29af6b62f1f627773fcb5195dde4bd7599b33474
ankitkundra <ankitkundra8@gmail.com>
2019-03-15 22:50:13 +0530

    Modals added

---
+++ stub/js/header.js
@@ -0,0 +1,39 @@
+var SignInBtn=document.getElementById("btn1");
+
+var SignInModal=document.getElementById("SignInModal");
+
+var close1 =document.getElementById("close1");
+
+SignInBtn.onclick = function(){
+    console.log("btnworking");
+    SignInModal.style.display ="block";
+}
+
+close1.onclick = function(){
+    SignInModal.style.display ="none";
+}
+
+window.onclick = function(event) {
+  if (event.target == SignInModal) {
+    SignInModal.style.display = "none";
+  }
+  else if (event.target == SignUpModal) {
+    SignUpModal.style.display = "none";
+  }
+}
+
+var SignUpBtn=document.getElementById("btn2");
+
+var SignUpModal=document.getElementById("SignUpModal");
+
+var close2 =document.getElementById("close2");
+
+SignUpBtn.onclick = function(){
+    console.log("btnworking");
+    SignUpModal.style.display ="block";
+}
+
+close2.onclick = function(){
+    SignUpModal.style.display ="none";
+}
+
